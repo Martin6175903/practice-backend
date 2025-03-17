b2b3273e4ae1ceafc4adbe319fcd77bc2e3e60ab
Martin6175903 <martin6175903@yandex.ru>
2025-03-17 22:29:56 +0300

    Added prisma in project and setting

---
+++ prisma/migrations/20250317192115_create_user_modal/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT[],
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import authRoutes from './auth/auth.routes.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { prisma } from './prisma.js';
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-function main() {
+async function main() {
 	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
@@ -19,4 +20,10 @@ function main() {
 	})
 }
 
-main()
+main().then(async() => {
+	await prisma.$disconnect()
+}).catch(async err => {
+	console.error(err)
+	await prisma.$disconnect()
+	process.exit(1)
+})
